4cb5ae8330ac150a6b7bbb71d49f3a397399df62
kseggelke918 <katie.seggelke@gmail.com>
2020-06-10 16:11:26 -0600

    correct issue with database persistence

--- react-project-backend/db/seeds.rb
+++ react-project-backend/db/seeds.rb
@@ -7,6 +7,6 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user = User.create(name: 'Katie')
-account = Account.create(acct_number: 12345, acct_type: 'checking', balance: 100.00)
-transaction_one = Transaction.create(amount: 55.32, debit_credit: 'debit', merchant: 'amazon', date: Date.today, account_id: 1)
-transaction_two = Transaction.create(amount: 10.00, debit_credit: 'credit', merchant: 'direct deposit', date: Date.today, account_id: 1)
+account = Account.create(acct_number: 12345, acct_type: 'checking', balance: 100.00, user_id: 1)
+transaction_one = Transaction.create(amount: 55.32, debit_credit: 'debit', merchant: 'amazon', date: Date.today, account_id: 1, user_id: 1)
+transaction_two = Transaction.create(amount: 10.00, debit_credit: 'credit', merchant: 'direct deposit', date: Date.today, account_id: 1, user_id: 1)
