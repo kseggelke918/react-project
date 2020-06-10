8c0e4250b0af62a2fe81af054e7017fab311a56b
kseggelke918 <katie.seggelke@gmail.com>
2020-06-10 15:18:21 -0600

    setting up accounts and transactions controller

--- react-project-backend/app/controllers/api/v1/accounts_controller.rb
+++ react-project-backend/app/controllers/api/v1/accounts_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::AccountsController < ApplicationController
+    before_action :set_user
+
+    def index 
+        account = Account.all 
+        render json: account
+    end 
+
+    def create
+        account = Account.new(account_params)
+        if accout.save 
+            render json: account
+        else 
+            render json: {error: "Account not created"}
+        end 
+    end 
+
+    def show 
+        account = Account.find(params[id])
+        render json: account 
+    end 
+
+    def destroy 
+        account = Account.find(params[id])
+        account.destroy 
+    end
+
+    private 
+
+    def set_user
+        @useer = User.find(params[:user_id])
+    end 
+
+    def account_params 
+        params.require(:account).permit(:acct_number, :acct_type, :balance, :user_id)
+    end 
+
 end
--- react-project-backend/app/controllers/api/v1/transactions_controller.rb
+++ react-project-backend/app/controllers/api/v1/transactions_controller.rb
@@ -1,2 +1,38 @@
 class Api::V1::TransactionsController < ApplicationController
+    before_action :set_account
+
+
+
+
+    def index 
+        transaction = Transaction.all 
+        render json: transaction
+    end 
+
+    def create 
+        transaction = Transaction.new(transaction_params)
+        if transaction.save 
+            render json: transaction
+        else 
+            render json: transaction
+        end 
+    end 
+
+    def show 
+
+    end 
+
+    def destroy 
+
+    end 
+
+    private 
+
+    def set_account 
+        @account = Account.find(params[:account_id]) 
+    end 
+
+    def transaction_params 
+        params.require(:transaction).permit(:amount, :debit_credit, :merchant, :date, :account_id)
+    end 
 end
