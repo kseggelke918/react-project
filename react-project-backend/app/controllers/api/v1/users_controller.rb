90bedaeb933b9f13375a8b5861ea9c24a65a0507
kseggelke918 <katie.seggelke@gmail.com>
2020-06-17 15:01:42 -0600

    add session login to user#create controller

--- react-project-backend/app/controllers/api/v1/users_controller.rb
+++ react-project-backend/app/controllers/api/v1/users_controller.rb
@@ -11,6 +11,7 @@ class Api::V1::UsersController < ApplicationController
     @user = User.new(user_params)
   
     if @user.save
+      session[:user_id] = @user.id 
       render json: UserSerializer.new(@user)
     else
       render json: {error: @user.errors.full_messages.to_sentence }, status: :unprocessable_entity
