91c4871bae00c1dfa77cf871d92ea8de514ff9e8
kseggelke918 <katie.seggelke@gmail.com>
2020-06-17 22:02:19 -0600

    format home page

--- react-project-frontend/src/components/Home.js
+++ react-project-frontend/src/components/Home.js
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom'
 
 const Home = () => (
     <div>
-        <h3>Welcome to MyTown Bank! Please <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link></h3>
+        <h3>Welcome to MyTown Bank!</h3>
+        <h4>Please <Link to="/signup">Sign Up</Link> or <Link to="/login">Log In</Link></h4>
     </div>
 )
 
