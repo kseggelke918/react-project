9d2de74ed4f712da3fa1b5b49f082abb3b865565
kseggelke918 <katie.seggelke@gmail.com>
2020-06-24 19:53:26 -0600

    displaying transactions on account page

--- react-project-frontend/src/components/AccountCard.js
+++ react-project-frontend/src/components/AccountCard.js
@@ -1,15 +1,26 @@
 import React from 'react'
 
 const AccountCard = ({ account }) => {
+    let transactions = account.attributes.transactions
+    const trans = transactions.map(t => (<p key={t.id}>{t.merchant} - ${t.amount} - {t.debit_credit}</p>))
+    
+    console.log("the account card trans value is", trans)
     return (
-        <div>
+        
+        <div> 
             <h5>Account Number: {account.attributes.acct_number}</h5>
             <h5>Account Type: {account.attributes.acct_type}</h5>
             <h5>Balance: ${account.attributes.balance}</h5>
-            
+            <br></br>
+            <h5>Transactions: {trans}</h5>
+
+
         </div>
         
     )
 }
 
 export default AccountCard
+
+
+// const myAccounts = props.accounts.map(a => (<p key={a.id}><Link to={`/accounts/${a.id}`}>{a.attributes.acct_number} - {a.attributes.acct_type}</Link></p>))
