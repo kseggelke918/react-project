9fb667a9ca2aa68676b25c2d4e36fcc052a88e36
kseggelke918 <katie.seggelke@gmail.com>
2020-06-24 19:57:07 -0600

    removed commented out code and console.log

--- react-project-frontend/src/components/AccountCard.js
+++ react-project-frontend/src/components/AccountCard.js
@@ -2,9 +2,8 @@ import React from 'react'
 
 const AccountCard = ({ account }) => {
     let transactions = account.attributes.transactions
-    const trans = transactions.map(t => (<p key={t.id}>{t.merchant} - ${t.amount} - {t.debit_credit}</p>))
-    
-    console.log("the account card trans value is", trans)
+    const transaction = transactions.map(t => (<p key={t.id}>{t.merchant} - ${t.amount} - {t.debit_credit}</p>))
+  
     return (
         
         <div> 
@@ -12,7 +11,7 @@ const AccountCard = ({ account }) => {
             <h5>Account Type: {account.attributes.acct_type}</h5>
             <h5>Balance: ${account.attributes.balance}</h5>
             <br></br>
-            <h5>Transactions: {trans}</h5>
+            <h5>Transactions: {transaction}</h5>
 
 
         </div>
@@ -22,5 +21,3 @@ const AccountCard = ({ account }) => {
 
 export default AccountCard
 
-
-// const myAccounts = props.accounts.map(a => (<p key={a.id}><Link to={`/accounts/${a.id}`}>{a.attributes.acct_number} - {a.attributes.acct_type}</Link></p>))
