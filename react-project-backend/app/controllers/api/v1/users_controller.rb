a62fb6954edd8619ce190ece76677957746543df
kseggelke918 <katie.seggelke@gmail.com>
2020-09-07 14:51:39 -0600

    remove commented out code

--- react-project-backend/app/controllers/api/v1/users_controller.rb
+++ react-project-backend/app/controllers/api/v1/users_controller.rb
@@ -1,12 +1,10 @@
 class Api::V1::UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
-  # GET /users/1
   def show
     render json: UserSerializer.new(@user)
   end
 
-  # POST /users
   def create
     @user = User.new(user_params)
   
@@ -18,7 +16,6 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
   def update
     if @user.update(user_params)
       render json: UserSerializer.new(@user)
@@ -27,18 +24,16 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
   def destroy
     @user.destroy
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
+   
     def set_user
       @user = User.find(params[:id])
     end
 
-    # Only allow a trusted parameter "white list" through.
     def user_params
       params.require(:user).permit(:name, :username, :password, :password_confirmation)
     end
--- react-project-backend/app/serializers/account_serializer.rb
+++ react-project-backend/app/serializers/account_serializer.rb
@@ -1,6 +1,5 @@
 class AccountSerializer
   include FastJsonapi::ObjectSerializer
   attributes :acct_number, :acct_type, :balance, :user_id, :transactions
-  # has_many :transactions
-  # belongs_to :user
+
 end
