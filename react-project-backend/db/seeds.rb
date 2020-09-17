7bb1e928ddef9e60c5a7b72e7f64c65e2f19731b
kseggelke918 <katie.seggelke@gmail.com>
2020-09-17 15:17:13 -0600

    fix tyypo, drop database

--- react-project-backend/db/seeds.rb
+++ react-project-backend/db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 user_one = User.create(name: 'Katie', username: 'katieS', password: "password")
-user_tow = User.create(name: 'Susie', username: 'susietest', password: 'password')
+user_two = User.create(name: 'Susie', username: 'susietest', password: 'password')
 account_one = Account.create(acct_number: 12345, acct_type: 'checking', balance: 100.00, user_id: 1)
 account_two = Account.create(acct_number: 98706, acct_type: 'savings', balance: 50.00, user_id: 2)
 transaction_one = Transaction.create(amount: 55.32, debit_credit: 'debit', merchant: 'amazon', date: Date.today, account_id: 1, user_id: 1)
